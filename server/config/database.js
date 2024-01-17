6b7f1bf90fa2dcda14664af381e220ff5691c63f
Hoangvu <hoangvu1872k2@gmail.com>
2024-01-17 15:00:05 +0700

    push

--- server/config/database.js
+++ server/config/database.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const connectDatabase = async ()=>{
 
     try {
-        const databaseConfig = "mongodb://127.0.0.1/ECommerce";       
-        // const URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cwkxo3r.mongodb.net/?retryWrites=true&w=majority`
+        // const databaseConfig = "mongodb://127.0.0.1/ECommerce";       
+        const URL = `mongodb+srv://hoangvu1872k2:dGLxWXOI9SfmbBBe@cluster0.8v4gmeb.mongodb.net/?retryWrites=true&w=majority`
         const connect = await mongoose.connect(databaseConfig);
         // const connect = await mongoose.connect(URL);
         console.log(`da ket noi mongodb: ${connect.connection.host}`);
